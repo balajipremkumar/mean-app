e9810d2cd352bdde788e42ed1e0b96faa0a92e88
balajiPremkumar <balajipreamkumar@gmail.com>
2021-05-14 10:36:47 +0530

    get and post functionality implemented

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,62 @@
 const express =  require("express");
 const bodyParser =  require("body-parser");
 //const { ConsoleReporter } = require("jasmine");
+const mongoose = require('mongoose');
+const Post = require("./models/post");
 const app = express();
- 
+
+
+mongoose.connect("mongodb://127.0.0.1:27017",{ useNewUrlParser: true , useUnifiedTopology:true})
+.then(()=>{
+  console.log("connected to database");
+}).catch(()=>{
+  console.log("connection failed");
+}) 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 
-app.use((req,res,next)=> {
-   res.setHeader("Access-control-Allow-Origin","*");
-   res.setHeader("Access-control-Allow-Headers","Origin, X-Requested-With, content-Type, Accept");
-   res.setHeader("Access-control-Allow-Methods","GET","POST","PATCH","PUT","DELETE","OPTIONS");
+
+
+app.use((req,res,next) => {
+   res.setHeader("Access-Control-Allow-Origin","*");
+   res.setHeader("Access-Control-Allow-Methods","GET","POST","PATCH","PUT","DELETE","OPTIONS");
+   res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, content-Type, Accept, append,delete,entries,foreach,get,has,keys,set,values,Authorization");
    next()
 });
 
 app.post("/api/posts", (req, res, next)=> {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({ 
+    title: req.body.title,
+    content:req.body.content
+  })
+  post.save()
   res.status(201).json({
     message: 'Post Added successfully'
   });
 });
 
 app.get('/api/posts',(req,res,next)=> { 
-  const posts = [{
-    id:'aas12323we',
-    title: 'First server-side post',
-    content: 'static data from server'
-  }]
-  
-  return res.status(200).json({
-    message:'post fetched successfully',
-    posts:posts
-  });
+  Post.find().then(documents =>{
+    res.status(200).json({
+      message:'post fetched successfully',
+      posts: documents
+    });
+  })
 })
+
+// app.delete("/api/posts/:id",(req, res, next)=> {
+//  console.log(req.params.id);
+//  res.status(200).json({ message: "Post deleted!" });
+// })
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!" });
+  });
+});
+
+
 
 module.exports = app; 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators'
 import { HttpClient } from '@angular/common/http'
 
 import{Post} from './post.model'
@@ -14,9 +15,18 @@ export class PostsService {
   constructor(private http:HttpClient) { }
 
   getPost(){
-     this.http.get<{message:string,posts:Post[]}>('http://localhost:3000/api/posts')
-     .subscribe((postData)=> {
-       this.posts =  postData.posts;
+     this.http.get<{ message:string, posts:any }>('http://localhost:3000/api/posts')
+     .pipe(map((postData)=> {
+        return postData.posts.map(post =>{
+          return {
+            title: post.title,
+            content: post.content,
+            id:post._id
+          }
+        })
+     }))
+     .subscribe((transformedPosts)=> {
+       this.posts =  transformedPosts;
        this.postUpdated.next([...this.posts]);
      });
   }
@@ -28,12 +38,19 @@ export class PostsService {
   addPost(title:string,content:string){
     console.log("addpost called ")
     const post:Post = {id:null, title:title, content:content};
-    this.http.post<{message:string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{message:string}>("http://localhost:3000/api/posts", post)
     .subscribe((responseData)=> {
        console.log(responseData.message);
        this.posts.push(post);
        this.postUpdated.next([...this.posts]);
     })
     
+  }
+
+  deletePost(postId:string){
+     this.http.delete("http://localhost:3000/api/posts/" + postId)
+     .subscribe(()=>{
+       console.log("Deleted");
+     })
   }
 }
